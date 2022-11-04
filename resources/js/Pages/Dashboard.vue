5c4b1b7bf556abc46434d637d6a51d05ce426c58
takami <takami.rodriguez@gmail.com>
2022-11-04 00:42:22 -0600

    Commit

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -50,15 +50,15 @@ defineProps(["users"])
                                     <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 text-white">
                                         <span class="sr-only">Actions</span>
                                     </th>
-                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 text-white">
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold  text-white">
                                         Name
                                     </th>
 
-                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 text-white">
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">
                                         Email
                                     </th>
 
-                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 text-white">
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">
                                         Created at
                                     </th>
 
