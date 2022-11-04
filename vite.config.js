55792e144be9e5b88656b7ab09d63db276738aaa
takami <takami.rodriguez@gmail.com>
2022-11-04 00:22:15 -0600

    Commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     server: {
         https: true,
-        host: '0.0.0.0',
+        host: 'localhost',
     },
     plugins: [
         laravel({
